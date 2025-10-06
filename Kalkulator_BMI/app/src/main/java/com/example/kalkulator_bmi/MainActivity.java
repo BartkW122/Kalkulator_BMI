ebf5343f0b8972628a71db4171fba83cc5d60907
Bartek <bartoszwiadernylodz@gmail.com>
2025-10-06 15:48:01 +0200

    skonczenie zadania

--- Kalkulator_BMI/app/src/main/java/com/example/kalkulator_bmi/MainActivity.java
+++ Kalkulator_BMI/app/src/main/java/com/example/kalkulator_bmi/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.kalkulator_bmi;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -8,50 +9,113 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-    private EditText waga,wzrost;
-    private Button btn_oblicz;
+    private EditText waga, wzrost;
+    private Button btn_oblicz,btn_wyczysc;
+    private TextView wynik,interpretacja;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        waga=findViewById(R.id.waga);
-        wzrost=findViewById(R.id.wzrost);
+        waga = findViewById(R.id.waga);
+        wzrost = findViewById(R.id.wzrost);
 
-        btn_oblicz=findViewById(R.id.btn_oblicz);
+        btn_oblicz = findViewById(R.id.btn_oblicz);
+        btn_wyczysc = findViewById(R.id.btn_wyczysc);
 
-        String error_empty,error_values;
+        wynik=findViewById(R.id.wynik);
+        interpretacja=findViewById(R.id.intepretacja);
 
-        error_empty=getResources().getString(R.string.error_empty_fields);
-
-        error_values=getResources().getString(R.string.error_zero_values);
         btn_oblicz.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String waga_info,wzrost_info;
-                waga_info=waga.getText().toString().trim();
-
-                wzrost_info=wzrost.getText().toString().trim();
-
-                Double wzrost_info_double;
-
-                //wzrost_info_double=Double.valueOf(wzrost_info);
-                if(wzrost_info.equals("") || waga_info.equals("")){
-                    Toast.makeText(MainActivity.this,error_empty, Toast.LENGTH_LONG).show();
-                }
-                /*if (wzrost_info_double <= 0) {
-                    Toast.makeText(MainActivity.this,error_values, Toast.LENGTH_LONG).show();
-                }*/
-
+                calculateBmi();
             }
         });
 
+        btn_wyczysc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                waga.setText("");
+                wzrost.setText("");
+
+                wynik.setText(R.string.bmi_result_prefix);
+                interpretacja.setText("");
+            }
+        });
+
+    }
+
+    private void calculateBmi(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        String waga_info, wzrost_info;
+
+        waga_info = waga.getText().toString().trim();
+        wzrost_info = wzrost.getText().toString().trim();
+
+        if (wzrost_info.equals("") || waga_info.equals("")) {
+
+            builder.setTitle("Błąd walidacji").setMessage(R.string.error_empty_fields).setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    dialogInterface.dismiss();
+                }
+            }).create().show();
+
+        }else {
+
+            Double wzrost_info_double, waga_info_double;
+
+            wzrost_info_double = Double.valueOf(wzrost_info);
+            waga_info_double = Double.valueOf(waga_info);
+
+            if (wzrost_info_double <= 0 || waga_info_double <= 0) {
+                builder.setTitle("Błąd walidacji").setMessage(R.string.error_zero_values).setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                }).create().show();
+            }
+
+            Double wynik_BMI=waga_info_double / Math.pow(wzrost_info_double / 100.0,2);
+
+            if(wynik_BMI < 18.5){
+
+                interpretacja.setText(R.string.bmi_underweight);
+
+                interpretacja.setTextColor(getResources().getColor(R.color.status_bad,null));
+
+            } else if (wynik_BMI >= 18.5 && wynik_BMI <= 24.9) {
+
+                interpretacja.setText(R.string.bmi_normal);
+
+                interpretacja.setTextColor(getResources().getColor(R.color.status_good,null));
+
+            } else if(wynik_BMI >= 25.0 && wynik_BMI <= 29.9){
+
+                interpretacja.setText(R.string.bmi_overweight);
+
+                interpretacja.setTextColor(getResources().getColor(R.color.status_warning,null));
+
+            } else if(wynik_BMI >= 30.0){
+
+                interpretacja.setText(R.string.bmi_obesity);
+
+                interpretacja.setTextColor(getResources().getColor(R.color.status_bad,null));
+
+            }
+            wynik.setText("Twoje BMI wynosi "+String.format("%.2f", wynik_BMI));
+
+        }
     }
 }
